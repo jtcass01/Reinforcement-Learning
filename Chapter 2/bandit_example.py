0a599613e31d31b5fc09c2deb449c21b23fda681
Jacob Cassady <jtcass01@louisville.edu>
2022-11-27 10:51:36 -0500

    Added prospector and pre-commits

--- Chapter 2/bandit_example.py
+++ Chapter 2/bandit_example.py
@@ -1,7 +1,7 @@
 from numpy import array, where, amax, nan_to_num
 from typing import List, Tuple
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     k: int = 4
     initial_estimate: float = 0.
     action_value_estimates: array = nan_to_num(array([initial_estimate]*k, dtype=float))
@@ -16,26 +16,28 @@ if __name__ == "__main__":
         action_index: int = action - 1
 
         # Display information for reference
-        print(f"previous action_value_estimates: {action_value_estimates}")
-        print(f"current Action({time_step}) = {action_index} -> Reward = {reward}")
+        print(f'previous action_value_estimates: {action_value_estimates}')
+        print(f'current Action({time_step}) = {action_index} -> Reward = {reward}')
 
         # Categorize action
-        print(f"{list(where(action_value_estimates == amax(action_value_estimates)))}")
-        if action_index in where(action_value_estimates == amax(action_value_estimates))[0]:
+        max_indices: array = where(action_value_estimates == amax(action_value_estimates))[0]
+        print(f'actions with best reward: {max_indices}')
+        if action_index in max_indices:
             unknown_timesteps.append(time_step)
         else:
             epsilon_timesteps.append(time_step)
 
         # Update
         action_counts[action_index] += 1
-        action_value_estimates[action_index] += (1/action_counts[action_index])*(reward-action_value_estimates[action_index]) ## Added after reading Section 2.4
+        action_value_estimates[action_index] += (1/action_counts[action_index])\
+                                                *(reward-action_value_estimates[action_index]) ## Added after reading Section 2.4
 
         ## Old update before incremental improvement:
         ##  reward_sums[action_index] += reward
         ##  action_value_estimates[action_index] = reward_sums[action_index] / action_counts[action_index]
 
-        print("")
+        print('')
 
     # Display result to Exercise 2.2:
-    print(f"On which time steps did this definitely occur? Answer: {epsilon_timesteps}")
-    print(f"On which time steps did this could this poissibly have occurred? Answer: {unknown_timesteps}")
+    print(f'On which time steps did this definitely occur? Answer: {epsilon_timesteps}')
+    print(f'On which time steps did this could this poissibly have occurred? Answer: {unknown_timesteps}')
